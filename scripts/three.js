f16d58abe329ccda88c234d4800b158690865531
severin13 <severin.baschung@gmail.com>
2020-01-15 20:46:39 +0100

    textured cube

--- scripts/three.js
+++ scripts/three.js
@@ -1,29 +1,58 @@
-// three.js setup
+let camera, scene, renderer, cube;
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+function init() {
+	// Init scene
+	scene = new THREE.Scene();
 
-var renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+	// Init camera (PerspectiveCamera)
+	camera = new THREE.PerspectiveCamera(
+		75,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000
+	);
 
+	// Init renderer
+	renderer = new THREE.WebGLRenderer({alpha: true}, { antialias: true });
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+	renderer.setSize(window.innerWidth, window.innerHeight);
 
-camera.position.z = 4;
-renderer.render( scene, camera );
+	// Render to canvas element
+	document.body.appendChild(renderer.domElement);
 
-var animate = function () {
-    requestAnimationFrame( animate );
+	// Init BoxGeometry object (rectangular cuboid)
+	const geometry = new THREE.BoxGeometry(2, 2, 2);
+	// Add texture
+	const texture = new THREE.TextureLoader().load('textures/cardboard_severin.jpg');
+	// Create material with texture
+	const material = new THREE.MeshBasicMaterial({ map: texture });
+	// Create material with color
+	//const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+	// Create mesh with geo and material
+	cube = new THREE.Mesh(geometry, material);
+	scene.add(cube);
 
-    renderer.render( scene, camera );
-};
+	camera.position.z = 5;
+}
 
+// Draw the scene every time the screen is refreshed
+function animate() {
+	requestAnimationFrame(animate);
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
 animate();
-
